95ac7fc2517437f32839598976f9427b5d9f6706
Sourinda SIVILAY <s.sivilay@live.fr>
2026-09-26 15:14:59 +0200

    feat: check channel name

--- src/rules/commands/Join.cpp
+++ src/rules/commands/Join.cpp
@@ -13,13 +13,32 @@ static std::vector<std::string> split(const std::string& str, char sep)
     {
         result.push_back(str.substr(start, pos - start));
         start = pos + 1;
+        pos = str.find(sep, start);
     }
     result.push_back(str.substr(start));
     return result;
 }
 
+static bool isValidChannelName(const std::string& name)
+{
+    if (name.length() < 2 || name.length() > 50)
+        return false;
+
+    if (name[0] != '#')
+        return false;
+
+    for (size_t i = 1; i < name.length(); ++i)
+    {
+        if (name[i] == ' ' || name[i] == ',' || name[i] == ':' || name[i] == '\x07')
+            return false;
+    }
+    return true;
+}
+
 void cmdJoin(Client* client, const Command& cmd, ServerState& state)
 {
+    (void)state;
+
     if (!client)
         return;
 
@@ -31,6 +50,17 @@ void cmdJoin(Client* client, const Command& cmd, ServerState& state)
 
     std::vector<std::string> channels = split(cmd.params[0], ',');
     std::vector<std::string> keys;
+
     if (cmd.params.size() > 1)
         keys = split(cmd.params[1], ',');
+
+    for (std::vector<std::string>::size_type i = 0; 
+            i < channels.size(); ++i)
+    {
+        if (!isValidChannelName(channels[i]))
+        {
+            reply(client, 403, channels[i] + " :No such channel");
+            continue;
+        }
+    }
 }
